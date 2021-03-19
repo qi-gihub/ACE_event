25d3dcb41fc5ceaf58ac782a91e945c2fc2cb14e
qi-gihub <78106140+qi-gihub@users.noreply.github.com>
2021-03-18 21:55:46 -0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -8,7 +8,7 @@ $conn = mysqli_connect($server,$username, $password, $dbname);
 
 if(isset($_POST['submit'])){
     
- if(!empty($_POST['fullName']) && 
+ if(!empty($_POST['name']) && 
     !empty($_POST['age']) && 
     !empty($_POST['gender']) && 
     !empty($_POST['email'] && 
@@ -21,9 +21,9 @@ if(isset($_POST['submit'])){
     !empty($_POST['sundayEvent'] && 
     !empty($_POST['role'] && 
     !empty($_POST['tshirtSize1']  && 
-    !empty($_POST['massege'])) {     
+    !empty($_POST['massege'])){    
     
-$fulalName = $_POST['FullName'];
+$name = $_POST['Name'];
 $age = $_POST['Age'];
 $gender = $_POST['Gender'];
 $email = $_POST['Email'];
@@ -39,10 +39,10 @@ $tshirtSize1 = $_POST['TshirtSize1'];
 $massege = $_POST['Massege'];
 
 
- $query = "ace into form(fullName, age, gender, email,
+ $query = "ace into form(name, age, gender, email,
           phonenumber, emergencyContactName,
           emEergencyContactNumber,saturdayEvent, role, tshirtSize,
-          sundayEvent, role1, tshirtSize1, massege) values('$fullName' ,'$age', '$gender', '$email','$phonenumber' ,'$emergencyContactName',
+          sundayEvent, role1, tshirtSize1, massege) values('$name' ,'$age', '$gender', '$email','$phonenumber' ,'$emergencyContactName',
           '$emergencyContactNumber' ,'$saturdayEvent',
           '$role' ,'$tshirtSize' ,'$sundayEvent' ,'$role1' ,'$tshirtSize1' ,'$massege')";
     
@@ -52,8 +52,9 @@ $massege = $_POST['Massege'];
         echo "Form submitted successfully";
     }
     else {
-       echo "Form not submitted" ;
+       echo "Form not submitted";
     }   
-}
+   }      
+ }
            
 ?>
--- tuch.php
+++ tuch.php
@@ -6,14 +6,14 @@ $dbname = "acecontact";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);if(isset($_POST['submit'])){
     
- if(!empty($_POST['fullName']) && !empty($_POST['email']) && !empty($_POST['role']) && !empty($_POST['massege'])){     
+ if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['role']) && !empty($_POST['massege'])){     
     
-    $name =  $_POST['fullName'];
+    $name =  $_POST['name'];
     $email = $_POST['email'];
     $role = $_POST['role'];
     $mssege = $_POST['massege'];
     
-    $query = "contact into form(fullName,email,role,massege) values(' $name', '$email',
+    $query = "contact into form(name,email,role,massege) values('$name', '$email',
     '$role', '$massege')";
     
     $run = mysqli_query($conn,$query) or die (mysqli_error());
@@ -22,9 +22,9 @@ $conn = mysqli_connect($server, $username, $password, $dbname);if(isset($_POST['
         echo "Form submitted successfully";
     }
     else {
-       echo "Form not submitted" ;
+       echo "Form not submitted";
     }   
 }
+}
+    
 ?>
-
-
